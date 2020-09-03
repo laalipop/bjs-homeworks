8a111e170c92132cf099b2361a00559b6186699f
laalipop <preicpodhya@mail.ru>
2020-09-03 10:53:06 +0300

    изменила функции в файле task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,46 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    let flag = 1;
+    let totalAmount;
+
+    if (Number(percent)) {
+        flag = 0;
+    } else {
+        totalAmount = (`Параметр "percent" содержит неправильное значение "${percent}"`);
+    }
+
+    if (Number(contribution)) {
+        flag = 0;
+    } else {
+        totalAmount = (`Параметр "contribution" содержит неправильное значение "${contribution}"`);
+    }
+
+    if (Number(amount)) {
+        flag = 0;
+    } else {
+        totalAmount = (`Параметр "amount" содержит неправильное значение "${amount}"`);
+    }
+
+    if (flag == 0) {
+        let returnToBanck = amount - contribution;
+        let numberOfPayments = (12 * (date.getFullYear() - new Date().getFullYear())) + (date.getMonth() - new Date().getMonth());
+        let interestRate = (percent / 12) / 100;
+        let paymentForMonth = returnToBanck * (interestRate + interestRate / (Math.pow((1 + interestRate), numberOfPayments) - 1));
+        totalAmount = paymentForMonth * numberOfPayments;        
+        totalAmount = totalAmount.toFixed(2);
+        totalAmount = Number(totalAmount);
+    }
+    return totalAmount;
 }
 
-function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
-}
+    function getGreeting(name) {
+        let greeting;
+        if (name === null || name === "" || name === undefined) {
+            greeting = `Привет, мир! Меня зовут Аноним`;
+        } else {
+              greeting = `Привет, мир! Меня зовут ${name}`;
+        }      
+        return greeting;
+    }
