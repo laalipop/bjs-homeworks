e3720b6a95e109991bc44a1a1b29a75591b41294
laalipop <66365113+laalipop@users.noreply.github.com>
2020-09-21 13:40:19 +0300

    внесены исправления

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -6,20 +6,17 @@ function parseCount(value) {
     value = Number.parseInt(value, 10);
 
     if (Number.isNaN(value)) {
-        const showError = new Error("Невалидное значение");
-        throw showError;
-    } else {
-        return value;
+        throw new Error("Невалидное значение");
     }
+    return value;  
 }
 
 function validateCount(valueElse) {
     try {
-        parseCount(valueElse);
-        return +valueElse;
+        //
+        return parseCount(valueElse);
     } catch (e) {
-        const showError = new Error("Невалидное значение");
-        return showError;
+        return e;
     }
 }
 
@@ -32,21 +29,16 @@ class Triangle {
         this.partC = partC;
 
         if (partA + partB < partC || partA + partC < partB || partB + partC < partA) {
-            const showError = new Error("Треугольник с такими сторонами не существует");
-            throw showError;
-        }
-        else {
-            console.log("все ок. это треугольник");
+            throw new Error("Треугольник с такими сторонами не существует");
         }
     }
 
     getPerimeter() {
-        const perimeter = this.partA + this.partB + this.partC;
-        return perimeter;
+        return this.partA + this.partB + this.partC;
     }
 
     getArea() {
-        const halfPerimeter = (this.partA + this.partB + this.partC) / 2;
+        const halfPerimeter = this.getPerimeter() / 2;
         const formulaGerona = Math.sqrt(halfPerimeter * (halfPerimeter - this.partA) * (halfPerimeter - this.partB) * (halfPerimeter - this.partC));
         return +formulaGerona.toFixed(3);
     }
@@ -54,15 +46,13 @@ class Triangle {
 
 function getTriangle(partA, partB, partC) {
     try {
-        const test = new Triangle(partA, partB, partC);
-        return test;
+        return new Triangle(partA, partB, partC);
     }
     catch (e) {
-        let returnObj = {
-            getPerimeter: function () { return "Ошибка! Треугольник не существует" },
-            getArea: function () { return "Ошибка! Треугольник не существует" }
+        return  {
+            getPerimeter: () => "Ошибка! Треугольник не существует",
+            getArea: () => "Ошибка! Треугольник не существует"
         }
-        return returnObj;
     }
 }
 //console.log(getTriangle(1, 300, 10));
